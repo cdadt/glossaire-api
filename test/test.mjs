aa5db79580e4d80623922ee3d9f569a1a308ed85
cdadt <->
2019-07-23 11:11:11 +0200

    essai travis

---
+++ test/test.mjs
@@ -0,0 +1,11 @@
+import chai from 'chai';
+
+const { assert } = chai;
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+  });
+});
