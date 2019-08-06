dad0838be5eff2c4ee10eb6fcdafca763e2c8c38
Azaguilla <laurie.m1@free.fr>
2019-08-06 14:06:32 +0200

    Ajout de la page d'ajout d'un thème avec upload d'image

--- models/theme.mjs
+++ models/theme.mjs
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 
 const ThemeSchema = new Schema({
   title: { type: String, required: true, unique: true },
+  img: { data: Buffer, contentType: String },
 });
 
 export default mongoose.model('Theme', ThemeSchema);
--- routes/themes.mjs
+++ routes/themes.mjs
@@ -1,8 +1,14 @@
 import Router from 'koa-router';
+import jwt from 'koa-jwt';
+import Multer from 'koa-multer';
 import Theme from '../models/theme';
+import config from '../services/config';
 import Word from '../models/word';
 
 const router = new Router();
+// const upload = Multer({ dest: 'uploads/' });
+const storage = Multer.memoryStorage();
+const upload = Multer({ storage });
 
 router.get('/', async (ctx) => {
   ctx.body = await Theme.find().lean();
@@ -32,6 +38,37 @@ router.get('/:id/words', async (ctx) => {
   const { id } = ctx.params;
   ctx.body = await Word.find({ 'themes._id': id }).lean();
 });
+
+router.post(
+  '/',
+  upload.single('image', 'png'),
+  jwt({ secret: config.get('token:secret') }),
+  async (ctx) => {
+    const newTheme = new Theme();
+
+    const themeTitle = ctx.req.body.title.trim();
+
+    newTheme.title = themeTitle;
+
+    if (ctx.req.file) {
+      newTheme.img.data = ctx.req.file.buffer;
+      newTheme.img.contentType = ctx.req.file.mimetype;
+    }
+
+    const theme = await Theme.findOne(
+      {
+        title:
+            {
+              $regex: themeTitle,
+              $options: 'i',
+            },
+      },
+    ).lean();
+
+    ctx.assert(!theme, 409, `Le thème '${themeTitle}' existe déjà.`);
+    ctx.body = await Theme.create(newTheme);
+  },
+);
 
 
 export default router.routes();
